a644a0304b24807e528688b9968cbc795192753a
Vishnu Kumar <77917126+Vishu7av@users.noreply.github.com>
2023-03-15 00:48:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim();
+}
+
+const sortedBands = bands.slice().sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+document.querySelector('#bands').innerHTML =
+  bands
+    .map(band => `<li>${band}</li>`)
+    .join('');
+
+document.querySelector('#bandsSorted').innerHTML =
+  sortedBands
+    .map(band => `<li>${band}</li>`)
+    .join('');
